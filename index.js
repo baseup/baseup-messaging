88dca320c3c29dcdeff241781fd7ec35f2e93132
GSPat1993 <gabrielp.lorax@gmail.com>
2018-05-25 14:37:55 +0800

    change procfile and query

--- index.js
+++ index.js
@@ -10,15 +10,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
    extended: false
 }));
-app.listen(3000, () => console.log("Webhook server is listening, port 3000"));
+app.listen(5000, () => console.log("Webhook server is listening, port 3000"));
 
 app.get('/', (req, res) => {
    res.send('HELLO WORLD');
 });
 
 app.get('/webhook', function (req, res) {
-   if (req.query['hub.mode'] === 'subscribe' &&
-      req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+   if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
       console.log("Validating webhook");
       res.status(200).send(req.query['hub.challenge']);
    } else {
