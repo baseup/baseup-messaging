3153ffd9c9b067b72e34446c685bb1a9f67978e5
GSPat1993 <gabrielp.lorax@gmail.com>
2018-05-25 15:19:10 +0800

    change keys to snake case

--- setup/facebook.js
+++ setup/facebook.js
@@ -2,19 +2,19 @@ const config = require('config');
 
 const APP_SECRET = (process.env.MESSENGER_APP_SECRET) ?
    process.env.MESSENGER_APP_SECRET :
-   config.get('appSecret');
+   config.get('app_secret');
 
 const VALIDATION_TOKEN = (process.env.MESSENGER_VALIDATION_TOKEN) ?
    (process.env.MESSENGER_VALIDATION_TOKEN) :
-   config.get('validationToken');
+   config.get('validation_toke');
 
 const PAGE_ACCESS_TOKEN = (process.env.MESSENGER_PAGE_ACCESS_TOKEN) ?
    (process.env.MESSENGER_PAGE_ACCESS_TOKEN) :
-   config.get('pageAccessToken');
+   config.get('access_token');
 
 const SERVER_URL = (process.env.SERVER_URL) ?
    (process.env.SERVER_URL) :
-   config.get('serverURL');
+   config.get('server_URL');
 
 if (!(APP_SECRET && VALIDATION_TOKEN && PAGE_ACCESS_TOKEN && SERVER_URL)) {
    console.error("Missing config values");
