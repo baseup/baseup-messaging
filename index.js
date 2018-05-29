5bd4dc8816c00e3e5930631120b9a7810601d11e
GSPat1993 <gabrielp.lorax@gmail.com>
2018-05-29 09:31:19 +0800

    adding functions on sending and handling message

--- index.js
+++ index.js
@@ -32,25 +32,81 @@ app.post('/webhook', (req, res) => {
    const body = req.body;
 
    if (body.object === 'page') {
-      body.entry.forEach((entry) => {
-         const id = entry.id;
-         const time = entry.time;
+      body.entry.forEach(function (entry) {
 
-         entry.messaging.forEach((event) => {
-            if (event.message) {
-               console.log(event.message);
-            } else {
-               console.log('Unknown Event: ', event);
-            }
-         });
          const webhook_event = entry.messaging[0];
-         console.log('WEBHOOK EVENT: ', entry);
+         console.log(webhook_event);
+
+         const sender_psid = webhook_event.sender.id;
+         console.log('Sender PSID: ' + sender_psid);
+
+         if (webhook_event.message) {
+            handleMessage(sender_psid, webhook_event.message);
+         } else if (webhook_event.postback) {
+            handlePostback(sender_psid, webhook_event.postback);
+         }
+
       });
       res.status(200).send('EVENT_RECEIVED');
    } else {
       res.sendStatus(404);
    }
 });
+
+function handleMessage(sender_psid, received_message) {
+   let response;
+
+   if (received_message.text) {
+
+      response = {
+         "text": `You sent the message: "${received_message.text}". Now send me an image!`
+      };
+   }
+
+   callSendAPI(sender_psid, response);
+}
+
+function callSendAPI(sender_psid, response) {
+   let request_body = {
+      "recipient": {
+         "id": sender_psid
+      },
+      "message": response
+   };
+
+   request({
+      "uri": "https://graph.facebook.com/v2.6/me/messages",
+      "qs": {
+         "access_token": facebook.PAGE_ACCESS_TOKEN
+      },
+      "method": "POST",
+      "json": request_body
+   }, (err, res, body) => {
+      if (!err) {
+         console.log('message sent!');
+      } else {
+         console.error("Unable to send message:" + err);
+      }
+   });
+}
+
+function handlePostback(sender_psid, received_postback) {
+   let response;
+
+   let payload = received_postback.payload;
+
+   if (payload === 'yes') {
+      response = {
+         "text": "Thanks!"
+      }
+   } else if (payload === 'no') {
+      response = {
+         "text": "Oops, try sending another image."
+      };
+   }
+   // Send the message to acknowledge the postback
+   callSendAPI(sender_psid, response);
+}
 
 app.listen(app.get('port'), function () {
    console.log('Node app is running on port', app.get('port'));
