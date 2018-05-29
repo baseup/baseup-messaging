9a752e2f6d8f77ac15c72e52cfafa8fd670b0288
GSPat1993 <gabrielp.lorax@gmail.com>
2018-05-29 10:45:28 +0800

    update api version

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function callSendAPI(sender_psid, response) {
    };
 
    request({
-      "uri": "https://graph.facebook.com/v2.6/me/messages",
+      "uri": "https://graph.facebook.com/v2.9/me/messages",
       "qs": {
          "access_token": facebook.PAGE_ACCESS_TOKEN
       },
