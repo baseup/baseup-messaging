a52aea51efc1b13de43ff3ce73c45ecfd5ca8553
GSPat1993 <gabrielp.lorax@gmail.com>
2018-05-29 10:38:08 +0800

    change access token

--- index.js
+++ index.js
@@ -104,7 +104,6 @@ function handlePostback(sender_psid, received_postback) {
          "text": "Oops, try sending another image."
       };
    }
-   // Send the message to acknowledge the postback
    callSendAPI(sender_psid, response);
 }
 
