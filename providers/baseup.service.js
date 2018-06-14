49043756756bd68c63789a1570767604e13346cf
GSPat <gabrielp.lorax@gmail.com>
2018-06-14 10:08:46 +0800

    stringify body

--- providers/baseup.service.js
+++ providers/baseup.service.js
@@ -82,6 +82,7 @@ function storeUserPSID(authCode, id, attributes) {
                console.log('ERROR');
             } else if (response) {
                console.log('NOT ERROR');
+               console.log(JSON.stringify(body));
                // new JSONAPIDeserializer({
                //    keyForAttribute: 'snake_case'
                // }).deserialize(JSON.parse(body), (err, users) => {
