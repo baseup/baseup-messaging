8b6f365ff6a9380a89fa3515f34958effb187a6c
GSPat <gabrielp.lorax@gmail.com>
2018-07-06 09:39:42 +0800

    emoji package

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.post('/webhooks', (req, res) => {
 
                         if (webhook_event.message) {
                               console.log('MESSAGE: ', webhook_event.message.text);
-                              console.log('MESSAGE: ', emoji.find(webhook_event.message.text));
+                              console.log('MESSAGE: ', emoji.unifiedToHTML(webhook_event.message.text));
                               handleMessage(sender_psid, webhook_event.message);
                         } else if (webhook_event.postback) {
                               handlePostback(sender_psid, webhook_event.postback);
