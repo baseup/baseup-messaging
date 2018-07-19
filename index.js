50c9746073f34d63202accc15b6affb6794cfc40
GSPat <gabrielp.lorax@gmail.com>
2018-07-19 09:50:07 +0800

    remove logs in looping business

--- index.js
+++ index.js
@@ -273,7 +273,6 @@ function handleGetPartners(psid, payload) {
       baseupServ.getBusinesses().then((result) => {
             const businesses = [];
             const filterActive = result.filter((value) => {
-                  console.log(JSON.stringify(value));
                   return value.id === '2' || value.id === '33' || value.id === '31' || value.id === '43';
             });
 
